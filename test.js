837bbbed979462c0d23da49810c7e1747b8bd456
Luis Angel Alba Malagon <lalba10@icloud.com>
2025-02-14 19:49:50 +0000

    Proyecto pruebas unitarias terminado OK 14022025

--- test.js
+++ test.js
@@ -22,13 +22,25 @@ test("One euro should be 1.07 dollars", function () {
 })
 
 //Prueba que sirve para asegurarnos que la funcion fromDollarToYen ha sido correctamente implementada
-test("One dollar should be 154.375 Yenes", function () {
+test("One dollar should be 146.2616822429907 Yenes", function () {
     // Importo la funcion desde app.js
     const { fromDollarToYen } = require('./app.js');
     // Uso la función como debe ser usada
     const Yenes = fromDollarToYen(5);
     // Si 1 dolar son 154.375 Yenes, entonces 5 dolares debe ser (5 * 154.375)
-    const expected = 5 * 154.375;
+    const expected = (5 / 1.07) * 156.5;
     // Hago mi comparación (la prueba)
-    expect(fromDollarToYen(5)).toBe(771.875); // 1 dolar son 154.375 yenes, entonces 5 dolares deberían ser = (5 * 154.375)
+    expect(fromDollarToYen(5)).toBe(731.3084112149533); // 5 dolares deberían ser = (731.3084112149533)
+})
+
+//Prueba que sirve para asegurarnos que la funcion fromTenToPuond ha sido correctamente implementada
+test("One Yen should be 0.00555910543413099 Pounds", function () {
+    // Importo la funcion desde app.js
+    const { fromYenToPound } = require('./app.js');
+    // Uso la función como debe ser usada
+    const Yenes = fromYenToPound(1000);
+    // Si 1 Yen son 0.0055591054313099, entonces 1000 Yenes debe ser (1000/156.5) * 0.87
+    const expected = (1000 / 156.5) * 0.87;
+    // Hago mi comparación (la prueba)
+    expect(fromYenToPound(1000)).toBe(5.559105431309905); // 1000 Yenes deberían ser = (5.559105431309904)
 })
